c68959992a092954c36031333db0f229763a1349
syldrew <syl_peke14@hotmail.com>
2023-10-18 15:36:23 -0500

    Tutorial Getting Started with Rails. http://guides.rubyonrails.org/, Chapters 1 to 6

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+  # Defines the root path route ("/")
+  get "/articles", to: "articles#index"
+
+  # root "articles#index"
+end
